95e9bc2ed5ab0ff705b037ec21d2486b4bc92bcd
wy <2972584338@qq.com>
2019-12-05 19:54:01 +0800

    test23

--- src/main/java/com/nyist/hello/mapper/TbBrandMapper.java
+++ src/main/java/com/nyist/hello/mapper/TbBrandMapper.java
@@ -7,6 +7,7 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 
 public interface TbBrandMapper {
+
     int countByExample(TbBrandExample example);
 
     int deleteByExample(TbBrandExample example);
